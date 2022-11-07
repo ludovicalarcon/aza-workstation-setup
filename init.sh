2bba605d063a331198989d7c11bbc5e5a96879da
ludovicalarcon <contact@ludovic-alarcon.com>
2022-11-07 21:51:23 +0100

    fix lint and add ansible lint on ci

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ then
   then
     echo "Installing Ansible"
     sudo pacman -Syu ansible --noconfirm
+    echo "Installing playbook community.general"
+    ansible-galaxy collection install community.general
   fi
   echo "Runing playbook"
   ansible-playbook workstation-setup.yaml -K
